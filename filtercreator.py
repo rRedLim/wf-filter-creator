e6c139a8cc65fd1168401cd2888a93521df95b08
rRedLim <yt.redlim@gmail.com>
2023-01-29 20:40:57 +0300

    added if __name__ == "__name__"

--- filtercreator.py
+++ filtercreator.py
@@ -59,25 +59,25 @@ sufixdict = {"punc": "cak",
 
 def get_riven_name(stat1,stat2,stat3):
     return f"{str(prefixdict.get(stat1)).capitalize()}-{prefixdict.get(stat2)}{sufixdict.get(stat3)}"
+if __name__ == "__main__":
+	string = ""
 
-string = ""
+	with open("input.txt", 'r') as f:
+		stats = [i.replace("\n", "").split() for i in f.readlines()]
 
-with open("input.txt", 'r') as f:
-	stats = [i.replace("\n", "").split() for i in f.readlines()]
+	for i in stats:
+		combinations = [' '.join(f) for f in permutations(i, 3)]
+		for v in combinations:
+			stat1 = v.split(' ')[0]
+			stat2 = v.split(' ')[1]
+			stat3 = v.split(' ')[2]
+			string += get_riven_name(stat1, stat2, stat3)+"\n"
 
-for i in stats:
-	combinations = [' '.join(f) for f in permutations(i, 3)]
-	for v in combinations:
-		stat1 = v.split(' ')[0]
-		stat2 = v.split(' ')[1]
-		stat3 = v.split(' ')[2]
-		string += get_riven_name(stat1, stat2, stat3)+"\n"
-
-zalupa = string.split("\n")
-string = ""
-for i in sorted(zalupa):
-	string += i+"\n"
- 
-with open("output.txt", 'w+') as f:
-	f.truncate()
-	f.write(string[1:])
+	zalupa = string.split("\n")
+	string = ""
+	for i in sorted(zalupa):
+		string += i+"\n"
+	
+	with open("output.txt", 'w+') as f:
+		f.truncate()
+		f.write(string[1:])
